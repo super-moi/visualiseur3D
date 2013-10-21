4bbc29997332ba9d03d49f39de5407d2c38386df
Thomas Ghesquiere <ghesquierethomas@gmail.com>
2013-10-21 18:57:09 +0200

    debut de codage

--- frame.adb
+++ frame.adb
@@ -9,8 +9,20 @@ package body Frame is
 
 	procedure Calcul_Image is
 	begin
-		-- a faire : calcul des projections, affichage des triangles
-		null;
+	   -- a faire : calcul des projections, affichage des triangles
+	   -- il commencer par remettre a zero l'affichage
+	   
+	   -- projection
+	   for I in M'Range loop
+	      Projection_Facettes(I);
+	   end loop;
+	   
+	   --affichage
+	   for J in M'Range loop
+	      for I in 1..3 loop
+		 Trace_Pixel(M(J)i);
+	      end loop;
+	   end loop;	   
 	end;
 
 end Frame;
